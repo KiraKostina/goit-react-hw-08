b9ebf8c6735de5f252a68882fcdd38d017499dd2
KiraKostina <kostina.kira@gmail.com>
2024-06-14 00:00:13 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,30 +1,30 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import ContactList from '../../components/ContactList/ContactList';
-// import css from './App.module.css';
-import SearchBox from './SearchBox/SearchBox';
-import ContactForm from '../../components/ContactForm/ContactForm';
-import { fetchContacts } from '../redux/contacts/operations';
-import Loader from './Loader/Loader';
-import ErrorMessage from './ErrorMessage/ErrorMessage';
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import ContactList from '../../components/ContactList/ContactList';
+// // import css from './App.module.css';
+// import SearchBox from './SearchBox/SearchBox';
+// import ContactForm from '../../components/ContactForm/ContactForm';
+// import { fetchContacts } from '../redux/contacts/operations';
+// import Loader from './Loader/Loader';
+// import ErrorMessage from './ErrorMessage/ErrorMessage';
 
-export default function ContactPage() {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(state => state.contacts.loading);
-  const isError = useSelector(state => state.contacts.error);
+// export default function ContactPage() {
+//   const dispatch = useDispatch();
+//   const isLoading = useSelector(state => state.contacts.loading);
+//   const isError = useSelector(state => state.contacts.error);
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(fetchContacts());
+//   }, [dispatch]);
 
-  return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      {isLoading && <Loader />}
-      {isError && <ErrorMessage />}
-      <ContactList />
-    </div>
-  );
-}
+//   return (
+//     <div className={css.container}>
+//       <h1>Phonebook</h1>
+//       <ContactForm />
+//       <SearchBox />
+//       {isLoading && <Loader />}
+//       {isError && <ErrorMessage />}
+//       <ContactList />
+//     </div>
+//   );
+// }
