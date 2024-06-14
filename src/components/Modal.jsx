ec659373d1533b233606521c45cde3117bc9acb5
KiraKostina <kostina.kira@gmail.com>
2024-06-15 00:57:23 +0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,32 @@
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  overlay: {
+    background: '#000000cd',
+  },
+};
+
+Modal.setAppElement('#root');
+
+export default function ConfirmModal() {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      style={customStyles}
+      contentLabel="Image Modal"
+    >
+      <img src={small} alt="regular" />
+    </Modal>
+  );
+}
